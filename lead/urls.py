a00a3170b966444e1bec304067a6c06c2626bce7
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-02-21 16:36:54 +0545

    feat: add lead delete and update functionality

--- lead/urls.py
+++ lead/urls.py
@@ -7,5 +7,7 @@ app_name = "lead"
 urlpatterns  =[
     path("create/", views.LeadCreateView.as_view(), name="leadCreate"),
     path("list/", views.LeadListView.as_view(), name="leadList"),
-    path("<int:pk>/detail/", views.LeadDetailView.as_view(), name="leadDetail")
+    path("<int:pk>/detail/", views.LeadDetailView.as_view(), name="leadDetail"),
+    path("<int:pk>/update/", views.LeadUpdateView.as_view(), name="leadUpdate"),
+    path("<int:pk>/delete/", views.LeadDeleteView.as_view(), name="leadDelete"),
 ]
--- lead/views.py
+++ lead/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 from django.urls import reverse_lazy, reverse
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.contrib.messages import success
+from django.contrib.messages.views import SuccessMessageMixin
 
 from lead.forms import LeadCreateForm
 from lead.models import Lead
@@ -38,3 +39,30 @@ class LeadDetailView(LoginRequiredMixin, DetailView):
     model = Lead
     context_object_name = "lead"
     template_name = "lead/leadDetail.html"
+
+
+class LeadUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin ,UpdateView):
+    model = Lead
+    form_class = LeadCreateForm
+    context_object_name = "lead"
+    template_name = "lead/leadUpdate.html"
+    success_message = "Lead Updated Successfully"
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        self.object = self.get_object()
+        return self.object.created_by == self.request.user
+    
+
+class LeadDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
+    model = Lead
+    template_name = "lead/leadDelete.html"
+    context_object_name = "lead"
+    success_message = "Lead Deleted Successfully"
+    success_url = reverse_lazy("lead:leadList")
+
+    def test_func(self):
+        self.object = self.get_object()
+        return self.object.created_by == self.request.user
